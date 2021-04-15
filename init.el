993d491d3a5917f8844017b1462bb0da0f191d68
rustyhu <rustyhu@hotmail.com>
2021-04-15 15:56:06 +0800

    Update my packages.

--- init.el
+++ init.el
@@ -3,20 +3,8 @@
 ;;;;; Basics
 ;; turn off backup file
 (setq make-backup-files nil)
-
-;;;;; GUI
-;; frame position
-(set-frame-position (selected-frame) 0 20)
-;; height and width
-(set-frame-width (selected-frame) 82)
-(set-frame-height (selected-frame) 35)
-;; bars off
-(tool-bar-mode -1) 
-(menu-bar-mode -1)
 ;; turn off welcome buffer
 (setq inhibit-startup-message t)
-
-;;;;; Text Format
 ;; mode line: line & column number
 (column-number-mode t)
 (line-number-mode t)
@@ -30,48 +18,59 @@
 (setq-default indent-tabs-mode nil)
 (setq default-tab-width 4)
 
+;;;;; GUI
+;; frame position
+(set-frame-position (selected-frame) 0 20)
+;; height and width
+(set-frame-width (selected-frame) 90)
+(set-frame-height (selected-frame) 45)
+;; bars off
+(tool-bar-mode -1) 
+(menu-bar-mode -1)
+
 ;;;;; Package Management
 (require 'package)
-
 ;; offical archive, reserved
 ;(add-to-list 'package-archives
 ;             '("melpa-stable" . "http://stable.melpa.org/packages/")
-;             '("marmalade" . "https://marmalade-repo.org/packages/"))
 ;; ELPA mirror of Emacs-China
-(setq package-archives '(("gnu"   . "http://elpa.zilongshanren.com/gnu/")
-                         ("melpa-stable" . "http://elpa.zilongshanren.com/melpa-stable/")
-;;                         ("melpa" . "http://elpa.zilongshanren.com/melpa/")
-                         ("Marmalade" . " 	http://elpa.zilongshanren.com/marmalade/")))
+(setq package-archives '(("gnu"   . "https://elpa.emacs-china.org/gnu/")
+                         ("melpa-stable" . "https://elpa.emacs-china.org/melpa-stable/")
+                         ("melpa" . "https://elpa.emacs-china.org/melpa/")))
 (package-initialize)
 
 ;; if need, refresh package pool cache 
 (when (not package-archive-contents)
   (package-refresh-contents))
-;;----- My Package List
-(defvar my-default-packages '(yasnippet
-	                          auto-complete
-                              evil
-                              monokai-theme
-                              smex))
-(dolist (p my-default-packages)
+
+(defvar my-packages '(smex
+                      evil
+                      monokai-theme
+                      ;doom-themes
+                      racket-mode
+                      auto-complete
+                      yasnippet))
+
+(dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-;; Specific settings of packages
-;;----- monokai theme
+
+;;;;; Settings of packages
 (load-theme 'monokai t)
-;;----- auto-complete
+;; auto-complete
 (require 'auto-complete-config)
 (ac-config-default)
-;;----- ido
+;; smex
 (require 'ido) 
 (ido-mode t)
-;;----- smex
 (require 'smex) 
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex) ; make smex triggered by M-x 
-;; MODE OFF by default when start
-;;----- Evil
-;(evil-mode 1)
-;;----- yasnippet
+;; Evil
+(evil-mode 1)
+
+;;;;; `customize` interface
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;(load custom-file)
 
 ;;------------------ END_CUSTOM ------------------;;
